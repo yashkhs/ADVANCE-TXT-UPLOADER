4b52eb2ee064192971260ccbbe3037b045a87c11
Tushar <tusartxt7570@gmail.com>
2025-02-02 13:38:42 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,1 +1,8 @@
+# Don't Remove Credit Tg - @Tushar0125
+# Ask Doubt on telegram @Tushar0125
 
+from os import environ
+
+API_ID = int(environ.get("API_ID", "28206084"))
+API_HASH = environ.get("API_HASH", "6b56f4cc8ef5750f51616cc22b78168d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
