633d4f132e20fe6597d25e30f6dce9e8a8bed9f1
Tushar <tusartxt7570@gmail.com>
2025-02-18 15:22:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,15 +38,6 @@ def is_authorized(user_id):
 def is_authorized(user_id: int) -> bool:
     return user_id == OWNER_ID or user_id in SUDO_USERS
 
-# Function to check if a channel is authorized
-def is_authorized(channel_id):
-    return channel_id in channel_USERS
-
-# Function to check if a channel is authorized
-def is_authorized(channel_id: int) -> bool:
-    return channel_id == channel_ID or channel_id in channel_USERS
-    
-
 bot = Client(
     "bot",
     api_id=API_ID,
@@ -89,53 +80,7 @@ async def sudo_command(bot: Client, message: Message):
     except Exception as e:
         await message.reply_text(f"**Error:** {str(e)}")
 
-#/add_channel
-@bot.on_message(filters.command("add_channel"))
-async def add_channel(client, message: Message):
-    channel_id = str(message.from_channel.id)
-    subscription_data = read_subscription_data()
-
-    if not any(user[0] == user_id for user in subscription_data):
-        await message.reply_text("You are not a premium user.")
-        return
-
-    try:
-        _, channel_id = message.text.split()
-        channels = read_channels_data()
-        if channel_id not in channels:
-            channels.append(channel_id)
-            write_channels_data(channels)
-            await message.reply_text(f"Channel {channel_id} added.")
-        else:
-            await message.reply_text(f"Channel {channel_id} is already added.")
-    except ValueError:
-        await message.reply_text("Invalid command format. Use: /add_channel <channel_id>")
-
-
-#/remove_channels
-@bot.on_message(filters.command("remove_channel"))
-async def remove_channel(client, message: Message):
-    channel_id = str(message.from_channel.id)
-    subscription_data = read_subscription_data()
-
-    if not any(user[0] == user_id for user in subscription_data):
-        await message.reply_text("You are not a premium user.")
-        return
-
-    try:
-        _, channel_id = message.text.split()
-        channels = read_channels_data()
-        if channel_id in channels:
-            channels.remove(channel_id)
-            write_channels_data(channels)
-            await message.reply_text(f"Channel {channel_id} removed.")
-        else:
-            await message.reply_text(f"Channel {channel_id} is not in the list.")
-    except ValueError:
-        await message.reply_text("Invalid command format. Use: /remove_channels <channel_id>")
-
 # Start command handler
-
 @bot.on_message(filters.command(["start"]))
 async def start(bot: Client, m: Message):
     editable = await m.reply_text(
