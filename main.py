cb3fee0308e0727e12ff96a761da1d1d87ce3649
Tushar <tusartxt7570@gmail.com>
2025-03-03 10:34:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,10 +38,10 @@ cookies_file_path = os.getenv("COOKIES_FILE_PATH", "youtube_cookies.txt")
 
 
 # Define the owner's user ID
-OWNER_ID = 5840594311 # Replace with the actual owner's user ID
+OWNER_ID = 7856557198 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [5840594311]
+SUDO_USERS = [6854709241,5840594311]
 
 AUTH_CHANNEL = -1002323619979
 
