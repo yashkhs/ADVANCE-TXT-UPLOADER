4e37d41f9582be1c5a6f7a7e2b50cd3311f51d98
Tushar <tusartxt7570@gmail.com>
2025-03-27 00:49:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -780,7 +780,7 @@ async def upload(bot: Client, m: Message):
             except Exception as e:
                 await m.reply_text(f'‼️𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗶𝗻𝗴 𝗙𝗮𝗶𝗹𝗲𝗱‼️\n\n'
                                    f'📝𝗡𝗮𝗺𝗲 » `{name}`\n\n'
-                                   f'🔗𝗨𝗿𝗹 » <a href="{link0}">__**Click Here to See Link**__</a>`')
+                                   f'🔗𝗨𝗿𝗹 » <a href="{url}">__**Click Here to See Link**__</a>`')
                                    
                 #count += 1
                 #failed_count += 1
