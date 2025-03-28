cd68cf6e4050a0a35c7f50dfd9213f67086c52c3
Tushar <tusartxt7570@gmail.com>
2025-03-28 22:46:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def show_random_emojis(message):
 OWNER_ID = 7856557198 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [6854709241,5840594311,7856557198]
+SUDO_USERS = [6854709241,5840594311,5745420142]
 
 AUTH_CHANNEL = -1002552787413
 
